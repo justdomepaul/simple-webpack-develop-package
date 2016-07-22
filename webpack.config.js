0b63df63552c85d6633571f9833efe9188e4d835
Maxfocker <max.focker.shih@gmail.com>
2016-07-22 17:32:26 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 module.exports = {
     entry: {
-        itpdbtableapp: './src/db-table/app.js'
+        dbtableapp: './src/db-table/app.js'
     },
     output: {
         path: path.join(__dirname, '/dist'),
